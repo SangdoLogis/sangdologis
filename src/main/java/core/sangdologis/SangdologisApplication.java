876272309eff5a5490545414efe56dcd3d54a6ca
w00smanK <pd11256@gmail.com>
2023-07-14 15:07:47 +0900

    [#1] Init : 최초 생성

---
+++ src/main/java/core/sangdologis/SangdologisApplication.java
@@ -0,0 +1,13 @@
+package core.sangdologis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangdologisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangdologisApplication.class, args);
+	}
+
+}
