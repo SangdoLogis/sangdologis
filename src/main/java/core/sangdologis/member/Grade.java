843a661c77eb929e03a62d11ad348fb6e3cef3da
w00smanK <pd11256@gmail.com>
2023-08-11 17:05:43 +0900

    [#4] docs : Grade 기준 및 git user test

--- src/main/java/core/sangdologis/member/Grade.java
+++ src/main/java/core/sangdologis/member/Grade.java
@@ -2,6 +2,11 @@ package core.sangdologis.member;
 
 public enum Grade {
 
+    /*
+     * SPECIAL : 초기/오픈 가입자만 해당
+     * BASIC : 일반 가입자
+     * VIP : 1년 이상 가입자
+     * */
     SPECIAL,
     BASIC,
     VIP,
