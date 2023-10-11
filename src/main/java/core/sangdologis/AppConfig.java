5b745c037ed61ff5735fa02b4067405fd9c80ea0
wookyoungK <pd1125@naver.com>
2023-10-11 23:49:48 +0900

    [#4] refactor : spring container 객체 등록

---
+++ src/main/java/core/sangdologis/AppConfig.java
@@ -0,0 +1,25 @@
+package core.sangdologis;
+
+import core.sangdologis.member.MemberRepository;
+import core.sangdologis.member.MemberService;
+import core.sangdologis.member.MemberServiceImpl;
+import core.sangdologis.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+* @Bean : 스프링 컨테이너(ApplicationContext)에 객체를 스프링 빈으로 등록
+*
+* */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
